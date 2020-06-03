e6a1129a6ef4f7767ef829362345630264fcc98e
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-06-03 09:24:52 +0200

    Initial commit

---
+++ src/main/java/com/jetbrains/dokka/sample/sellmair/SampleJavaClass.java
@@ -0,0 +1,8 @@
+package com.jetbrains.dokka.sample.sellmair;
+
+/**
+ * This is a sample Java class.
+ * Unlike {@link com.jetbrains.dokka.sample.selllmair.SampleKotlinClass}, this class is written in java
+ */
+public class SampleJavaClass {
+}
---
+++ src/main/kotlin/com/jetbrains/dokka/sample/selllmair/SampleKotlinClass.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.dokka.sample.selllmair
+
+import com.jetbrains.dokka.sample.sellmair.SampleJavaClass
+
+/**
+ * This is a sample Kotlin class
+ * Unlike [SampleJavaClass], this is written in Kotlin
+ */
+class SampleKotlinClass {
+}
